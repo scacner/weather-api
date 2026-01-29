ba7c7156c402b7c260e7b1d54bc6632d782b84e0
Steven Cacner <cacman14@gmail.com>
2026-01-29 16:37:31 -0500

    add initial api code and deps

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/scacner/weather-api
 
 go 1.25.6
+
+require (
+	github.com/swaggest/openapi-go v0.2.60
+	github.com/swaggest/rest v0.2.75
+	github.com/swaggest/swgui v1.8.5
+	github.com/swaggest/usecase v1.3.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/santhosh-tekuri/jsonschema/v3 v3.1.0 // indirect
+	github.com/swaggest/form/v5 v5.1.1 // indirect
+	github.com/swaggest/jsonschema-go v0.3.78 // indirect
+	github.com/swaggest/refl v1.4.0 // indirect
+	github.com/vearutop/statigz v1.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/scacner/weather-api/internal/nwsapi"
+	"github.com/swaggest/openapi-go/openapi3"
+	"github.com/swaggest/rest/web"
+	swgui "github.com/swaggest/swgui/v5emb"
+	"github.com/swaggest/usecase"
+	"github.com/swaggest/usecase/status"
+)
+
+// Main function to set up and start the Weather API service.
+func main() {
+	s := web.NewService(openapi3.NewReflector())
+
+	// Init API documentation schema
+	s.OpenAPISchema().SetTitle("Weather API")
+	s.OpenAPISchema().SetDescription("Uses the NWS API to provide weather information.")
+	s.OpenAPISchema().SetVersion("v0.0.1")
+
+	// Declare input type
+	type currentInput struct {
+		Latitude  float64 `query:"latitude" description:"Latitude of the latitude"`
+		Longitude float64 `query:"longitude" description:"Longitude of the latitude"`
+	}
+
+	// Declare output type
+	type currentOutput struct {
+		CurrentShortForecast string `json:"current_short_forecast"`
+		CurrentTemperature   string `json:"current_temperature"`
+	}
+
+	// Create use case interactor with references to input/output types and interaction function.
+	u := usecase.NewInteractor(func(ctx context.Context, input currentInput, output *currentOutput) error {
+		// Example: Rochester, NY approx coords (43.1567, -77.6148)
+		forecast, err := nwsapi.GetNWSForecast(input.Latitude, input.Longitude)
+		if err != nil {
+			return status.Wrap(err, status.Internal)
+		}
+
+		// Print today's forecast as example
+		if len(forecast.Properties.Periods) > 0 {
+			period := forecast.Properties.Periods[0]
+			output.CurrentShortForecast = period.ShortForecast
+			output.CurrentTemperature = fmt.Sprintf("%.0f", period.Temperature)
+		}
+
+		return nil
+	})
+
+	// Describe use case interactor
+	u.SetTitle("Current Weather")
+	u.SetDescription("Get current weather information for given latitude and longitude.")
+
+	u.SetExpectedErrors(status.Internal)
+
+	// Add use case handler to router
+	s.Get("/current", u)
+
+	// Swagger UI endpoint at /docs
+	s.Docs("/docs", swgui.New)
+
+	// Start server
+	log.Println("API Spec: http://localhost:8080/docs")
+	if err := http.ListenAndServe("localhost:8080", s); err != nil {
+		log.Fatal(err)
+	}
+}
